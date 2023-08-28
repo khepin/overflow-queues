5c2c7703fea07d8452b55e929f8f0cb4a5e70816
Seb <sebastien@squareup.com>
2023-08-28 09:21:24 -0700

    command description

--- app/Console/Commands/QueuePlentyOfJobs.php
+++ app/Console/Commands/QueuePlentyOfJobs.php
@@ -9,6 +9,9 @@ class QueuePlentyOfJobs extends Command
 {
     protected $signature = 'app:queue-plenty-of-jobs';
 
+    protected $description = 'Queues a bunch of jobs for a "big user" that will take long to execute
+    and a bunch of jobs for smaller users with random ids.';
+
     public function handle()
     {
         for ($i = 0; $i < 100; $i++) {
